42c3069e3a9ea583e687e1ce66b4fb37577487c3
vesahs <118637407+vesahs@users.noreply.github.com>
2022-12-01 22:11:07 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -80,4 +80,17 @@ var swiper = new Swiper(".menu-slider", {
       el: ".swiper-pagination",
       clickable: true,
    },
+   
 });
+
+const sr = ScrollReveal ({
+   distance : '45px',
+   duration : 3000,
+   reset : false,
+ })
+
+
+ sr.reveal('.menu-food-content',{ delay:250, origin:'top' })
+ sr.reveal('.menu-food-text',{ delay:250, origin:'top' })
+ sr.reveal('.food-main-content',{ delay:250, origin:'bottom' })
+ sr.reveal('.before',{ delay:250, origin:'bottom' })
